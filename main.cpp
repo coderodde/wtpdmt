8c5071b440561b95df7cc5676c9ce0ad09cf5275
coderodde <coderodd3@gmail.com>
2023-10-13 16:32:04 +0300

    Some work on main.cpp.

--- main.cpp
+++ main.cpp
@@ -17,6 +17,41 @@
 
 using std::string;
 
+// BEGIN OF: https://stackoverflow.com/a/17387176/1049393
+//Returns the last Win32 error, in string format. Returns an empty string if there is no error.
+string GetLastErrorAsString()
+{
+	//Get the error message ID, if any.
+	DWORD errorMessageID = GetLastError();
+
+	if (errorMessageID == 0) {
+		return string(); //No error message has been recorded
+	}
+
+	LPSTR messageBuffer = nullptr;
+
+	//Ask Win32 to give us the string version of that message ID.
+	//The parameters we pass in, tell Win32 to create the buffer that holds the message for us (because we don't yet know how long the message string will be).
+	size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+								 FORMAT_MESSAGE_FROM_SYSTEM     | 
+								 FORMAT_MESSAGE_IGNORE_INSERTS,
+								 NULL,
+								 errorMessageID, 
+								 MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), 
+								 (LPSTR)& messageBuffer,
+								 0, 
+								 NULL);
+
+	//Copy the error message into a std::string.
+	string message(messageBuffer, size);
+
+	//Free the Win32's string's buffer.
+	LocalFree(messageBuffer);
+
+	return message;
+}
+// END OF: https://stackoverflow.com/a/17387176/1049393
+
 int main(int argc, char* argv[]) try {
 	com::github::coderodde::wtpdmt::util::CommandLineParser clp(argc, argv);
 
@@ -66,7 +101,28 @@ int main(int argc, char* argv[]) try {
 		      << clp.getThreadPriority()
 		      << ")\n";
 
-	clp.printHelp();
+	HANDLE process_handle = GetCurrentProcess();
+	HANDLE thread_handle  = GetCurrentThread();
+
+	if (!SetPriorityClass(process_handle, clp.getPriorityClass())) {
+		std::cerr << "WARNING: Could not set the process class. Windews returned: "
+		          << GetLastErrorAsString() 
+				  << ".\n";
+	}
+
+	if (!SetThreadPriority(thread_handle, clp.getThreadPriority())) {
+		std::cerr << "WANRING: Could not set the thread priority. Windows returned: "
+		          << GetLastErrorAsString()
+				  << ".\n";
+	}
+
+	std::cout << "INFO: Effective process class is   " 
+			  << clp.getPriorityClassName(GetPriorityClass(process_handle))
+			  << ".\n";
+
+	std::cout << "INFO: Effective thread priority is "
+			  << clp.getThreadPriorityName(GetThreadPriority(thread_handle))
+			  << ".\n";
 
 	ULONGLONG ta = GetTickCount64();
 	ULONGLONG maximumDuration = 0;
